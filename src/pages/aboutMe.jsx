84bcedcc4ab5758dee8b6326ce8eddf05cbc33bf
kutho53 <kurtisthomas53@gmail.com>
2025-02-16 22:52:14 -0500

    aboutme update

--- src/pages/aboutMe.jsx
+++ src/pages/aboutMe.jsx
@@ -3,7 +3,7 @@ import image from '../assets/Stick_Figure.svg.png';
 function AboutMe(){
   return (
     <section class='container mt-5' id='aboutMe'>
-      <h2 style={{marginTop: '2em'}}>About me</h2>
+      <h2 style={{marginTop: '2em', marginBottom: '1em'}}>About me</h2>
       <div class='row'>
         <div class='col-sm-4'>
           <img src={image} alt='stick figure' style={{ width: '200px', height: '200px', backgroundColor: 'pink', borderRadius: 100 }} />
