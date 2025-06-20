9c44a7d34531babeddfebf67316e56122384c855
kutho53 <kurtisthomas53@gmail.com>
2025-06-19 23:39:27 -0400

    Update portfolio-page.jsx

--- src/pages/portfolio-page.jsx
+++ src/pages/portfolio-page.jsx
@@ -9,23 +9,23 @@ export default function Portfolio(){
             <section>
                 <div class='row'> 
                     <div class='col-md card'>
-                        <h3 class='card-header'>Project Example 1</h3>
+                        <h3 class='card-header'>Tic-Tac-Toe</h3>
                         <div class='card-body'>
                             <img src={image} alt='Sample Image' style={{ width: '200px', height: '200px', backgroundColor: 'blue', borderRadius: 100 }} />
                         </div>
                         <div class='card-footer'>
-                            <a href='https://github.com/kutho53/README-Generator'>Github Link </a>
-                            <a href='https://kutho53.github.io/README-Generator/' >Deployed Link</a>
+                            <a href='https://github.com/kutho53/TicTacToe'>Github Link </a>
+                            <a href='https://github.com/kutho53/TicTacToe' >Deployed Link</a>
                         </div>   
                     </div>
                     <div class='col-md card'>
-                        <h3 class='card-header'>Project Example 1</h3>
+                        <h3 class='card-header'>WeatherStorm</h3>
                         <div class='card-body'>
                             <img src={image} alt='Sample Image' style={{ width: '200px', height: '200px', backgroundColor: 'blue', borderRadius: 100 }} />
                         </div>                                        
                         <div class='card-footer'>
-                            <a href='https://github.com/kutho53/README-Generator'>Github Link </a>
-                            <a href='https://kutho53.github.io/README-Generator/' >Deployed Link</a>
+                            <a href='https://github.com/kutho53/WeatherSite'>Github Link </a>
+                            <a href='https://github.com/kutho53/WeatherSite' >Deployed Link</a>
                         </div>   
                     </div>
                     <div class='col-md card'>
