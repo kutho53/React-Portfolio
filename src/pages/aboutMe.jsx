4eb8cbeabb9405e66b124601a260ac63ce73e0a1
kutho53 <kurtisthomas53@gmail.com>
2025-06-29 00:09:57 -0400

    aboutMe formatting updates

--- src/pages/aboutMe.jsx
+++ src/pages/aboutMe.jsx
@@ -3,15 +3,16 @@ import image from '../assets/Screenshot_20250623_235626_Gallery.jpg';
 function AboutMe(){
   return (
     <section class='container mt-5' id='aboutMe'>
-      <h2 style={{marginTop: '2em', marginBottom: '1em'}}>About me</h2>
+      
       <div class='row'>
         <div class='col-sm-4'>
-          <img src={image} alt='stick figure' style={{ width: '200px', height: '200px', backgroundColor: 'pink', borderRadius: 100 }} />
+          <img src={image} alt='stick figure' style={{ width: '270px', height: '370px', backgroundColor: 'pink', borderRadius: 10 }} />
         </div>
         <div class='col-sm-8'>
+          <h2 style={{marginTop: '0.5em', marginBottom: '1em'}}>About me</h2>
           <p>
           
-            I'm a recent graduate of UNC-Chapel Hill's Software Development program with a passion for building practical, efficient, and user-focused solutions. My journey into software development began after earning a bachelor's degree in Business and working hands-on as an aviation technician—an experience that shaped my attention to detail, problem-solving mindset, and commitment to precision under pressure.
+            I'm a recent graduate of UNC-Chapel Hill's Software Development program with a passion for building practical, efficient, and user-focused solutions. My journey into software development began after earning a bachelor's degree in Business and working hands-on as a Marine Corps aviation technician—an experience that shaped my attention to detail, problem-solving mindset, and commitment to precision under pressure.
           </p><p>
             Transitioning into tech felt like a natural evolution. I enjoy breaking down complex challenges and crafting clean, functional code that brings ideas to life. My background gives me a unique perspective: I understand both the technical and business sides of a project, and I thrive in environments where collaboration, adaptability, and continuous learning are key.
           </p><p>
