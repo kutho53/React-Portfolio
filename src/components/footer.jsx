79f0709b7a1453ec03b58790ae56d3c91a8fe9a5
kutho53 <kurtisthomas53@gmail.com>
2025-06-29 00:32:11 -0400

    footer formatting updates

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,13 @@
-export default function footer(){
+import './components.css'
+
+export default function Footer(){
     return(
-        <footer class="mt-5 p-4 bg-dark text-white text-center">Github: <a href='https://github.com/kutho53'>Kutho53</a> LinkedIn: <a href='https://linkedin.com/in/kurt-thomas-912168169/'>Kurt Thomas</a></footer>
+        <footer className="mt-5 p-4 bg-dark text-white text-center">Github: <a href='https://github.com/kutho53'>Kutho53</a> LinkedIn: <a href='https://linkedin.com/in/kurt-thomas-912168169/'>Kurt Thomas</a></footer>
     )
 }
 
 <div class="mt-5 p-4 footer-expand bg-dark text-white text-center">
   <p>Footer</p>
 </div>
+
+// mt-5 p-4 bg-dark text-white text-center
