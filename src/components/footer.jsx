aa51930fed6e4b537ede1784c73a10554cb42978
kutho53 <kurtisthomas53@gmail.com>
2025-07-06 23:59:21 -0400

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import './components.css'
 
 export default function Footer(){
     return(
-        <footer className="mt-5 p-4 bg-dark text-white text-center">Github: <a href='https://github.com/kutho53'>Kutho53</a> LinkedIn: <a href='https://linkedin.com/in/kurt-thomas-912168169/'>Kurt Thomas</a></footer>
+        <footer className="mt-5 p-4 bg-dark text-white text-center">Github: <a href='https://github.com/kutho53'>Kutho53</a> <img> <a href='https://linkedin.com/in/kurt-thomas-912168169/'>Kurt Thomas</a></footer>
     )
 }
 
