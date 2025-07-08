07bc6957ff6b54f77a668bd1efb48342ba36d637
kutho53 <kurtisthomas53@gmail.com>
2025-07-07 23:53:32 -0400

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,10 @@
 import './components.css'
+import LILogo from '../assets/3-1.png';
+import GHLogo from '../assets/';
 
 export default function Footer(){
     return(
-        <footer className="mt-5 p-4 bg-dark text-white text-center">Github: <a href='https://github.com/kutho53'>Kutho53</a> <img src='./src/assets/3-1.png'> </img> <a href='https://linkedin.com/in/kurt-thomas-912168169/'>Kurt Thomas</a></footer>
+        <footer className="mt-5 p-4 bg-dark text-white text-center">Github: <a href='https://github.com/kutho53'>Kutho53</a> <img src='LILogo'> </img> <a href='https://linkedin.com/in/kurt-thomas-912168169/'>Kurt Thomas</a></footer>
     )
 }
 
